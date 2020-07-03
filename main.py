345ab5f4994cbfd97c862ebc173b7d6cbdc062ff
mridulganga <mridul.kepler@gmail.com>
2020-07-03 20:59:09 +0530

    add procfile and cors

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import connexion
+from flask_cors import CORS
+
 
 app = connexion.FlaskApp(__name__, specification_dir='openapi/')
 app.add_api('swagger.yaml')
+
+# add CORS support
+CORS(app.app)
+
 app.run(port=8080)
